4df759db6ced67ab8cc614fc5e713b27f387c9f4
Samara Souza <samyesouza13@gmail.com>
2025-01-29 11:12:05 -0300

    testing styles

--- app/App.js
+++ app/App.js
@@ -1,13 +1,16 @@
 import { StyleSheet } from 'react-native';
-import { WebView } from 'react-native-webview';
+import { WebView, View } from 'react-native-webview';
 import Constants from 'expo-constants';
 
 export default function App() {
   return (
-    <WebView
-      style={styles.container}
+    <View style={styles.container}> 
+      <WebView
+      style={styles.webview}
       source={{ uri: 'https://frontend-cvxux7qv9-samssouza22s-projects.vercel.app' }}
-    />
+      />
+    </View>
+    
   );
 }
 
@@ -15,5 +18,11 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     marginTop: Constants.statusBarHeight,
+    backgroundColor: '#f0f0f0', // Cor de fundo do contêiner
+    padding: 10, // Espaçamento interno do contêiner
+  },
+  webview: {
+    flex: 1, // Faz a WebView ocupar todo o espaço disponível no contêiner
+    borderRadius: 10, // Bordas arredondadas para a WebView
   },
 });
