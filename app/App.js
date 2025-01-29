29ae6a4065f7a4f1de9b6bf87372f792a9b0281f
Samara Souza <samyesouza13@gmail.com>
2025-01-29 11:51:21 -0300

    redefining previows changes

--- app/App.js
+++ app/App.js
@@ -1,28 +1,19 @@
 import { StyleSheet } from 'react-native';
-import { WebView, View } from 'react-native-webview';
+import { WebView } from 'react-native-webview';
 import Constants from 'expo-constants';
 
 export default function App() {
   return (
-    <View style={styles.container}> 
-      <WebView
-      style={styles.webview}
+    <WebView
+      style={styles.container}
       source={{ uri: 'https://frontend-cvxux7qv9-samssouza22s-projects.vercel.app' }}
-      />
-    </View>
-    
+    />
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 4,
+    flex: 1,
     marginTop: Constants.statusBarHeight,
-    backgroundColor: '#f0f0f0', // Cor de fundo do contêiner
-    padding: 10, // Espaçamento interno do contêiner
-  },
-  webview: {
-    flex: 1, // Faz a WebView ocupar todo o espaço disponível no contêiner
-    borderRadius: 10, // Bordas arredondadas para a WebView
   },
 });
